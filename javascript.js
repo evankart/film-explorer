8668db936049574eb1a5c5361d4f0406300ba35b
Evan Kartheiser <ekarthei@gmail.com>
2022-11-30 11:26:54 -0800

    manual reset

--- javascript.js
+++ javascript.js
@@ -110,54 +110,60 @@ async function createURL() {
   let filmStockTerm = filmStock.value.trim();
   gallery.innerHTML = "";
   let URLlist = [];
-  // let page = `page=${Math.floor(Math.random() * 3) + 1}`;
-  let page = 1;
-  fullURL = `${baseURL}${methodPhotoSearch}&api_key=${api_key}${JSON}&${tagURL}${searchTerm},${filmStockTerm},${cameraTerm},&${page}`;
-  console.log(fullURL);
-  const response = await fetch(fullURL)
-    .then((response) => response.json())
-    .then((data) => data);
   for (let i = 0; i < 15; i++) {
-    let object = data.photos.photo[randomIndex];
-    console.log("OBJECT: ", object);
-    let serverID = object.server;
-    let photoID = object.id;
-    let secret = object.secret;
-    let imgUrl = `https://live.staticflickr.com/${serverID}/${photoID}_${secret}.jpg`;
-    while (URLlist.includes(imgUrl)) {
-      randomIndex = Math.floor(Math.random() * data.photos.photo.length);
-    }
-    URLlist.push(randomIndex); // track which images have been added
-
-    // console.log(imgUrl);
-    let newFig = document.createElement("figure");
-    let newCaption = document.createElement("figcaption");
-    let newLink = document.createElement("a");
-
-    let newImg = document.createElement("img");
-    newFig.appendChild(newImg);
-    newFig.appendChild(newCaption);
-    newCaption.appendChild(newLink);
-    newImg.src = imgUrl;
-    gallery.appendChild(newFig);
-    let infoURL = `${baseURL}${methodGetInfo}&api_key=${api_key}${JSON}&photo_id=${photoID}`;
-    fetch(infoURL)
+    // let page = `page=${Math.floor(Math.random() * 3) + 1}`;
+    let page = 1;
+    fullURL = `${baseURL}${methodPhotoSearch}&api_key=${api_key}${JSON}&${tagURL}${searchTerm},${filmStockTerm},${cameraTerm},&${page}`;
+    console.log(fullURL);
+    const response = await fetch(fullURL)
       .then((response) => response.json())
       .then((data) => {
-        let fullLink = data.photo.urls.url[0]._content;
-        let authorName = data.photo.owner.realname;
-        let username = data.photo.owner.username;
-        console.log(fullLink);
-        newLink.href = fullLink;
-        newLink.target = "_blank";
-        if (authorName !== "") {
-          newLink.textContent = `${authorName}`;
-        } else {
-          newLink.textContent = `${username}`;
+        let randomIndex = Math.floor(Math.random() * data.photos.photo.length);
+        // pick a different number if this image has already been used
+        console.log("DATA: ", data);
+        let object = data.photos.photo[randomIndex];
+        console.log("OBJECT: ", object);
+        let serverID = object.server;
+        let photoID = object.id;
+        let secret = object.secret;
+        let imgUrl = `https://live.staticflickr.com/${serverID}/${photoID}_${secret}.jpg`;
+        while (URLlist.includes(imgUrl)) {
+          randomIndex = Math.floor(Math.random() * data.photos.photo.length);
         }
-        console.log("author name:", authorName, data);
-      })
-      .catch((error) => console.log("Error fetching and parsing data.", error));
+        URLlist.push(randomIndex); // track which images have been added
+
+        // console.log(imgUrl);
+        let newFig = document.createElement("figure");
+        let newCaption = document.createElement("figcaption");
+        let newLink = document.createElement("a");
+
+        let newImg = document.createElement("img");
+        newFig.appendChild(newImg);
+        newFig.appendChild(newCaption);
+        newCaption.appendChild(newLink);
+        newImg.src = imgUrl;
+        gallery.appendChild(newFig);
+        let infoURL = `${baseURL}${methodGetInfo}&api_key=${api_key}${JSON}&photo_id=${photoID}`;
+        fetch(infoURL)
+          .then((response) => response.json())
+          .then((data) => {
+            let fullLink = data.photo.urls.url[0]._content;
+            let authorName = data.photo.owner.realname;
+            let username = data.photo.owner.username;
+            console.log(fullLink);
+            newLink.href = fullLink;
+            newLink.target = "_blank";
+            if (authorName !== "") {
+              newLink.textContent = `${authorName}`;
+            } else {
+              newLink.textContent = `${username}`;
+            }
+            console.log("author name:", authorName, data);
+          })
+          .catch((error) =>
+            console.log("Error fetching and parsing data.", error)
+          );
+      });
   }
 }
 
